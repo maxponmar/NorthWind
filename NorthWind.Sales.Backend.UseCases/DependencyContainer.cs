4c01de0d3a3f0cb67606e63aa73fd94152dfc416
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-13 18:11:34 -0700

    Se agrega configuracion de injeccion de dependencias

---
+++ NorthWind.Sales.Backend.UseCases/DependencyContainer.cs
@@ -0,0 +1,11 @@
+namespace NorthWind.Sales.Backend.UseCases;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection AddUseCasesServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICreateOrderInputPort, CreateOrderInteractor>();
+        
+        return services;
+    }
+}
--- NorthWind.Sales.Backend.UseCases/GlobalUsings.cs
+++ NorthWind.Sales.Backend.UseCases/GlobalUsings.cs
@@ -1,6 +1,8 @@
 // Global using directives
 
+global using Microsoft.Extensions.DependencyInjection;
 global using NorthWind.Sales.Backend.BusinessObjects.Aggregates;
 global using NorthWind.Sales.Backend.BusinessObjects.Interfaces.CreateOrder;
 global using NorthWind.Sales.Backend.BusinessObjects.Interfaces.Repositories;
+global using NorthWind.Sales.Backend.UseCases.CreateOrder;
 global using NorthWind.Sales.Entities.Dtos.CreateOrder;
