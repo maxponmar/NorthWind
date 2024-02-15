46bd82c81765a336ad0afcd3deacf3420182343b
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-14 21:16:45 -0700

    Se crea controlador para crear ordenes

---
+++ NorthWind.Sales.Backend.Controllers/CreateOrder/CreateOrderController.cs
@@ -0,0 +1,19 @@
+namespace Microsoft.AspNetCore.Builder;
+
+public static class CreateOrderController
+{
+    public static WebApplication UseCreateOrderController(this WebApplication app)
+    {
+        app.MapPost(Endpoints.CreateOrder, CreateOrder);
+
+        return app;
+    }
+    public static async Task<int> CreateOrder(
+        CreateOrderDto orderDto, 
+        ICreateOrderInputPort inputPort, 
+        ICreateOrderOutputPort presenter)
+    {
+        await inputPort.Handle(orderDto);
+        return presenter.OrderId;
+    }
+}
---
+++ NorthWind.Sales.Backend.Controllers/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using NorthWind.Sales.Backend.BusinessObjects.Interfaces.CreateOrder;
+global using NorthWind.Sales.Entities.Dtos.CreateOrder;
+global using NorthWind.Sales.Entities.ValueObjects;
