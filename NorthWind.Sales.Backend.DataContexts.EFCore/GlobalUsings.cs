67134e8979a820948f288421d87ba9e2734f43c2
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-17 12:21:38 -0700

    Se crea contexto para produccion utilizando el patron Options

--- NorthWind.Sales.Backend.DataContexts.EFCore/GlobalUsings.cs
+++ NorthWind.Sales.Backend.DataContexts.EFCore/GlobalUsings.cs
@@ -2,5 +2,7 @@
 
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.Options;
 global using NorthWind.Sales.Backend.BusinessObjects.POCOEntities;
+global using NorthWind.Sales.Backend.DataContexts.EFCore.Options;
 global using NorthWind.Sales.Backend.Repositories.Entities;
