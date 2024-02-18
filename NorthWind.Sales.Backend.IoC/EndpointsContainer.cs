855ec7658d3e9b244e3c60a96658763b21df5f08
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-18 08:47:01 -0700

    Se agrega registro de endpoints

---
+++ NorthWind.Sales.Backend.IoC/EndpointsContainer.cs
@@ -0,0 +1,11 @@
+namespace Microsoft.AspNetCore.Builder;
+
+public static class EndpointsContainer
+{
+    public static WebApplication MapNorthWindSalesEndpoints(this WebApplication app)
+    {
+        app.UseCreateOrderController();
+        
+        return app;
+    }
+}
