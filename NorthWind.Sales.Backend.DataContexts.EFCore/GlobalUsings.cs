e60623df963c7b2672c3c61189241fdfcf4f879c
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-18 08:19:16 -0700

    Se crea implementacion de INorthWindSalesCommandsDataContext

--- NorthWind.Sales.Backend.DataContexts.EFCore/GlobalUsings.cs
+++ NorthWind.Sales.Backend.DataContexts.EFCore/GlobalUsings.cs
@@ -5,5 +5,7 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Options;
 global using NorthWind.Sales.Backend.BusinessObjects.POCOEntities;
+global using NorthWind.Sales.Backend.DataContexts.EFCore.DataContexts;
 global using NorthWind.Sales.Backend.DataContexts.EFCore.Options;
 global using NorthWind.Sales.Backend.Repositories.Entities;
+global using NorthWind.Sales.Backend.Repositories.Interfaces;
---
+++ NorthWind.Sales.Backend.DataContexts.EFCore/Services/NorthWindSalesCommandsDataContext.cs
@@ -0,0 +1,12 @@
+namespace NorthWind.Sales.Backend.DataContexts.EFCore.Services;
+
+internal class NorthWindSalesCommandsDataContext(IOptions<DBOptions> dbOptions) : 
+    NorthWindSalesContext(dbOptions), INorthWindSalesCommandsDataContext
+{
+    public async Task AddOrderAsync(Order order) => await AddAsync(order);
+
+    public async Task AddOrderDetailsAsync(IEnumerable<OrderDetail> orderDetails)
+        => await AddRangeAsync(orderDetails);
+
+    public async Task SaveChangesAsync() => await base.SaveChangesAsync();
+}
