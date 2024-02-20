3fa21f8f07e59d52cdd3d0b97cbb21510ff121f1
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-19 18:26:54 -0700

    Se crea contendor de dependencias para HttpClient usando fabrica de httpclients de Microsoft

--- NorthWind.Sales.Frontend.WebApiGateways/GlobalUsings.cs
+++ NorthWind.Sales.Frontend.WebApiGateways/GlobalUsings.cs
@@ -1,6 +1,7 @@
 // Global using directives
 
 global using System.Net.Http.Json;
+global using Microsoft.Extensions.DependencyInjection;
 global using NorthWind.Sales.Entities.Dtos.CreateOrder;
 global using NorthWind.Sales.Entities.ValueObjects;
 global using NorthWind.Sales.Frontend.BusinessObjects.Interfaces;
