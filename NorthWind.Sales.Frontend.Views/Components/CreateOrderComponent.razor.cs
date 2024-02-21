359e7e00a94520725a432c4f23ebd65de917e566
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-20 19:15:46 -0700

    Se crea componente de create order

---
+++ NorthWind.Sales.Frontend.Views/Components/CreateOrderComponent.razor.cs
@@ -0,0 +1,7 @@
+namespace NorthWind.Sales.Frontend.Views.Components;
+
+public partial class CreateOrderComponent
+{
+    [Parameter]
+    public CreateOrderViewModel Order { get; set; }
+}
--- NorthWind.Sales.Frontend.Views/GlobalUsings.cs
+++ NorthWind.Sales.Frontend.Views/GlobalUsings.cs
@@ -1,5 +1,7 @@
 // Global using directives
 
+global using Microsoft.AspNetCore.Components;
 global using NorthWind.Sales.Entities.Dtos.CreateOrder;
 global using NorthWind.Sales.Frontend.BusinessObjects.Interfaces;
 global using NorthWind.Sales.Frontend.Views.Resources;
+global using NorthWind.Sales.Frontend.Views.ViewModels.CreateOrder;
