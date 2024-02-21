52ff7b21d9b472248f6b13b5a8934734025ab464
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-20 20:12:13 -0700

    Se agregan global usings

---
+++ NorthWind.Sales.BlazorClient/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using NorthWind.Sales.BlazorClient;
+global using NorthWind.Sales.Frontend.IoC;
