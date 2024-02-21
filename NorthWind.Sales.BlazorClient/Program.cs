9f572ca1972977d00be8b7ec9b47e793f93ac0c2
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-02-20 20:11:26 -0700

    Se registran servicios Frontend y configuracion de http client

--- NorthWind.Sales.BlazorClient/Program.cs
+++ NorthWind.Sales.BlazorClient/Program.cs
@@ -1,11 +1,10 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using NorthWind.Sales.BlazorClient;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddNorthWindSalesServices(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["WebApiAddress"]);
+});
 
 await builder.Build().RunAsync();
